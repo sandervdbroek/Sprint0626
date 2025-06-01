303299ca84b98388477bf213e1d320f8bfbf590b
Fintan Healy <fintan.healy@bristol.ac.uk>
2025-06-01 16:17:03 +0100

    WIP on dynamicsTeam_0
    tweeking agruments

--- +scripts/rerun_sizing.m
+++ +scripts/rerun_sizing.m
@@ -1,19 +1,18 @@
-load('example_data\A220_simple.mat')
+load('C:\git\Sprint0626\example_data\UB321_simple.mat')
 
 %% ========================= Set Hyper-parameters =========================
 ADP.AR = 12;
-ADP.HingeEta = 0.7;
+ADP.HingeEta = 0.8;
 ADP.FlareAngle = 15;
-ADP.ADR.M_c = 0.78;
-ADP.SweepAngle = []; % if empty will link to mach number...
+ADP.ADR.M_c = 0.8;
+ADP.SweepAngle = 25;
 
 %% ============================ Re-run Sizing =============================
 % conduct sizing
 ads.util.printing.title('Example Surrogates','Length',60,'Symbol','$')
-SubHarmonic = [0.8,3000./cast.SI.Nmile];
 sizeOpts = util.SizingOpts(IncludeGusts=false,...
-    IncludeTurb=false,BinFolder='bin_size',SubHarmonic=SubHarmonic);
-[ADP,res_mtom,Lds,time,isError,Cases] = ADP.Aircraft_Sizing(sizeOpts,"SizeMethod","SAH");
+    IncludeTurb=false,BinFolder='bin_size');
+[ADP,res_mtom,Lds,time,isError,Cases] = ADP.Aircraft_Sizing(sizeOpts);
 % get data during cruise
 fh.printing.title('Get Cruise Loads','Length',60)
 [~,Lds_c]=ADP.StructuralSizing(...
@@ -26,10 +25,8 @@ if ~isfolder('example_data')
     mkdir('example_data');
 end
 
-save('example_data/A220_simple_rerun.mat','ADP','Lds');
+save('example_data/UB321_simple_rerun.mat','ADP','Lds');
 
 %% ======================== Get Mission Fuel Burn =========================
 meta = ADP.ToMeta;
-fh.printing.title('','Length',60,'Symbol','=')
-fh.printing.title(sprintf('Trip Fuel: %.3f t',meta.Fuel_trip./1e3),'Length',60,'Symbol','=')
-fh.printing.title(sprintf('MTOM: %.2f t',ADP.MTOM),'Length',60,'Symbol','=')
+fh.printing.title(sprintf('Trip Fuel: %.3f t',meta.Fuel_trip./1e3))
--- @TAW/addMass.m
+++
@@ -1,9 +0,0 @@
-function baffItm = addMass(obj, baffItm, mass, eta, offset, id)
-
-for m_idx = 1:length(mass)
-    massOb = baff.Mass(mass(m_idx),"eta",eta(m_idx),"Name",id{m_idx});
-    massOb.Offset = [0; offset(m_idx); 0];
-    baffItm.add(massOb);
-    clear massOb
-end
-end
