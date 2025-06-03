d29fc24e5e5b78763dc1e913558dd26b9579aade
JoeDec <jd15808@bristol.ac.uk>
2025-06-03 19:34:40 +0100

    Basic Shevell curve wave drag - provisions added to avoid asymptotic behaviour and fix at Mach 1

--- @TAW/UpdateAeroEstimates.m
+++ @TAW/UpdateAeroEstimates.m
@@ -1,4 +1,5 @@
 function obj = UpdateAeroEstimates(obj)
-    obj.AeroSurrogate = aero.NitaPolar(obj);
+    % obj.AeroSurrogate = aero.NitaPolar(obj);
+    obj.AeroSurrogate = aero.NitaShevellPolar(obj);
 end
 
